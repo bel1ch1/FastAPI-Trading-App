f507b1fefb2ebfd6c1e4c62509435c22d1f56355
bel1c1 <andreyzv5555@gmail.com>
2024-04-08 14:51:47 +0300

    fixed bug with naming

--- auth/schemas.py
+++ auth/schemas.py
@@ -12,7 +12,7 @@ class UserRead(schemas.BaseUser[int]):
     is_verified: Optional[bool] = False
 
     class Config:
-        orm_mode = True
+        from_attributes = True
 
 
 class UserCreate(schemas.BaseUserCreate):
