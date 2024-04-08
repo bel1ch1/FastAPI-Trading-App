cf67cae50b2ef324a2ea853ca59c57a92346ca19
bel1c1 <andreyzv5555@gmail.com>
2024-04-08 18:43:21 +0300

    added routers for operations

--- src/operations/router.py
+++ src/operations/router.py
@@ -1,4 +1,11 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from database import get_async_session
+from sqlalchemy import select, insert
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from operations.models import operation
+from operations.schemas import OperationCreate
+
 
 router = APIRouter(
     prefix="/operations",
@@ -7,5 +14,15 @@ router = APIRouter(
 
 
 @router.get("/")
-async def get_operations():
-    return
+async def get_specific_operations(operation_type: str, session: AsyncSession = Depends(get_async_session)):
+    query = select(operation).where(operation.c.type == operation_type)
+    result = await session.execute(query)
+    return result.all()
+
+
+@router.post("/")
+async def add_specific_operation(new_operation: OperationCreate, session: AsyncSession = Depends(get_async_session)):
+    stmt = insert(operation).values(**new_operation.dict())
+    await session.execute(stmt)
+    await session.commit()
+    return {"status": "success"}
